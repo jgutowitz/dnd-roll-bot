850d122a61aaa98231fb3101c6d87ff53622f268
nfcariello <ncariell@nyit.edu>
2020-01-13 18:58:29 -0500

    Additional updates

--- events/message.js
+++ events/message.js
@@ -1,6 +1,7 @@
 const kick = require('../commands/kick.js');
 const ping = require('../commands/ping.js');
 const addplayer = require('../commands/addplayer.js');
+const d20 = require('d20');
 
 module.exports = (client, message) => {
     if (message.content.startsWith('!kick')) {
@@ -14,11 +15,17 @@ module.exports = (client, message) => {
         message.reply(`${playerName}`);
         return addplayer(message, playerName)
     }
-    else if (message.content.startsWith('Hey Bot')) {
-        return message.reply('Fuck off baby')
+    else if (message.content.startsWith('!roll')) {
+        let dice = message.toString().split(" ")[1];
+        if (dice === undefined) {
+            return message.reply('Please specify the number side of dice you want to roll with.')
+        } else {
+            let result = d20.roll(dice);
+            return message.reply(`Your roll is ${result}`)
+        }
     }
     //must be final branch
     else if (message.content.startsWith('!')) {
         return message.reply(`I do not understand the command: ${message}`)
     }
-}
+};
